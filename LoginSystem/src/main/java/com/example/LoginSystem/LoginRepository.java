17655a1e6783511f1fe6b8fd856e9b1ba733c5de
kirbz1 <kirbywilliamsmith1@gmail.com>
2023-03-11 19:57:02 +1100

    added index, new html files + edited controller repo user classfiles

--- LoginSystem/src/main/java/com/example/LoginSystem/LoginRepository.java
+++ LoginSystem/src/main/java/com/example/LoginSystem/LoginRepository.java
@@ -1,9 +1,10 @@
 package com.example.LoginSystem;
 
-
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 @Repository
-public interface LoginRepository extends JpaRepository<User, int> {
+public interface LoginRepository extends JpaRepository<User, Integer> {
 
     
 
--- LoginSystem/src/main/java/com/example/LoginSystem/User.java
+++ LoginSystem/src/main/java/com/example/LoginSystem/User.java
@@ -1,6 +1,9 @@
 package com.example.LoginSystem;
 
-
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
 public class User {
