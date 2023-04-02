6a618897a4eb2d9e2b60c4c583f706ec44b5599a
kirbz1 <kirbywilliamsmith1@gmail.com>
2023-04-02 18:38:12 +1000

    added files

---
+++ LoginSystem/src/main/java/com/example/LoginSystem/Account.java
@@ -0,0 +1,52 @@
+package com.example.LoginSystem;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "account", schema = "public")
+public class Account {
+
+	@Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private int id;
+    private String username;
+    private String password;
+
+    public Account() {
+
+    }
+
+    public Account(int id, String username, String password) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public String getUsername() {
+        return this.username;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
