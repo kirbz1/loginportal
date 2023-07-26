07be5c2ba3095d6f12719ff33f72dd87db7d4ed6
kirbz1 <kirbywilliamsmith1@gmail.com>
2023-07-26 16:21:49 +1000

    added flash attribute to display unique welcome message upon login, no account verification implemented yet

--- LoginSystem/src/main/java/com/example/LoginSystem/LoginController.java
+++ LoginSystem/src/main/java/com/example/LoginSystem/LoginController.java
@@ -10,11 +10,14 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.bind.annotation.PathVariable;
-
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 
 @Controller
 public class LoginController {
+
+    private Account current_user;
+    private boolean logged_in;
 
     @Autowired
     private LoginService service;
@@ -23,10 +26,20 @@ public class LoginController {
     public String viewHomePage(Model model) {
         List<Account> accountlist = service.listAll();
         model.addAttribute("accountlist", accountlist);
-        System.out.print("Get / ");
+        if (this.logged_in) {
+            model.addAttribute("logged_in", true);
+            model.addAttribute("current_user", current_user);
+        }
+
         return "index";
     }
 
+    @GetMapping("/login")
+    public String login(Model model) {
+        model.addAttribute("current_user", new Account());
+        return "login";
+    }
+    
     @GetMapping("/new")
     public String add(Model model) {
         model.addAttribute("account", new Account());
@@ -36,6 +49,18 @@ public class LoginController {
     @RequestMapping(value = "/save", method = RequestMethod.POST)
     public String saveAccount(@ModelAttribute("account") Account account) {
         service.save(account);
+        return "redirect:/";
+    }
+
+    @RequestMapping(value = "/login", method = RequestMethod.POST)
+    public String attemptLogin(@ModelAttribute("current_user") Account account, final RedirectAttributes redirectAttributes) {
+        //find and verify account exists in repo
+        //handle negative case - doesn't exist
+        //positive case - create new signed in user element and go to home page?
+        redirectAttributes.addFlashAttribute("logged_in", true);
+		redirectAttributes.addFlashAttribute("current_user", account);
+        this.logged_in = true;
+        this.current_user = account;
         return "redirect:/";
     }
 
